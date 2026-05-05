32045cde6bfa586c5f3e8779ecb69bf02c57ede2
Laraib <laraibahmad052@gmail.com>
2026-05-05 10:52:51 +0500

    Fix User model path

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,10 +1,10 @@
-const User = require('../models/user.model');
+const User = require('../models/User.model'); // ✅ Capital U
 
 class UserRepository {
-  findByEmail(email)          { return User.findOne({ email }); }
-  findById(id)                { return User.findById(id); }
-  create(data)                { return User.create(data); }
-  updateFcmToken(id, token)   { return User.findByIdAndUpdate(id, { fcmToken: token }); }
+  findByEmail(email)        { return User.findOne({ email }); }
+  findById(id)              { return User.findById(id); }
+  create(data)              { return User.create(data); }
+  updateFcmToken(id, token) { return User.findByIdAndUpdate(id, { fcmToken: token }); }
 }
 
 module.exports = new UserRepository();
